1776c9a54f5d0f4c9116e9e40fd033c8cb82338e
Jayne Jacobs <jaynejacobs@comcast.net>
2017-11-18 20:13:07 +0000

    adding db files and related updates

--- app.js
+++ app.js
@@ -1,89 +1,103 @@
-var express = require("express");
-
-var app = express();
-var bodyParser = require("body-parser");
+var express   = require("express"),
+  mongoose    = require("mongoose"),
+  app         = express(),
+  bodyParser  = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");   
+mongoose.connect("mongodb://localhost/songs_list");
 
-var songs = [{
-         name: "Girls Just want to have fun",
-         link: "https://www.youtube.com/watch?v=PIb6AZdTr-A",
-         music: "https://drive.google.com/file/d/1FrluKZhsiIQbWQILkLZUERdXlGEANP_4/view?usp=sharing",
+
+var songSchema = new mongoose.Schema({
+    name: String,
+    link: String,
+    music: String,
+    index: Number
+});
+//Index  /songs    GET    Display a list of all songs
+var Song = mongoose.model("Song", songSchema);
+//Schema
+// Song.create(
+//           {
+//             name: "Somebody that I Used to Know",
+//             link: "https://youtu.be/DqRC5tquyU0",
+//             music: "https://docs.google.com/document/d/1k-M7YrSOcPEZZ8IFsORFLUt9Y7tV9xUBNOw58pByGe4/edit?usp=sharing",
+//             index: 122
+//          }, function (err, song) {
+//              if(err){
+//                  console.log("Failed to create song");
+                 
+//              }else{
+//              console.log("Success, created:");
+//              console.log(song);
+//              }
          
-         },
-         {
-         name: "Somebody that I Used to Know",
-         link: "https://youtu.be/DqRC5tquyU0",
-         music: "https://docs.google.com/document/d/1k-M7YrSOcPEZZ8IFsORFLUt9Y7tV9xUBNOw58pByGe4/edit?usp=sharing",
-         
-         },{
-         name: "Girls Just want to have fun",
-         link: "https://www.youtube.com/watch?v=PIb6AZdTr-A",
-         music: "https://drive.google.com/file/d/1FrluKZhsiIQbWQILkLZUERdXlGEANP_4/view?usp=sharing",
-         
-         },
-         {
-         name: "Somebody that I Used to Know",
-         link: "https://youtu.be/DqRC5tquyU0",
-         music: "https://docs.google.com/document/d/1k-M7YrSOcPEZZ8IFsORFLUt9Y7tV9xUBNOw58pByGe4/edit?usp=sharing",
-         
-         },{
-         name: "Girls Just want to have fun",
-         link: "https://www.youtube.com/watch?v=PIb6AZdTr-A",
-         music: "https://drive.google.com/file/d/1FrluKZhsiIQbWQILkLZUERdXlGEANP_4/view?usp=sharing",
-         
-         },
-         {
-         name: "Somebody that I Used to Know",
-         link: "https://youtu.be/DqRC5tquyU0",
-         music: "https://docs.google.com/document/d/1k-M7YrSOcPEZZ8IFsORFLUt9Y7tV9xUBNOw58pByGe4/edit?usp=sharing",
-         
-         },{
-         name: "Girls Just want to have fun",
-         link: "https://www.youtube.com/watch?v=PIb6AZdTr-A",
-         music: "https://drive.google.com/file/d/1FrluKZhsiIQbWQILkLZUERdXlGEANP_4/view?usp=sharing",
-         
-         },
-         {
-         name: "Somebody that I Used to Know",
-         link: "https://youtu.be/DqRC5tquyU0",
-         music: "https://docs.google.com/document/d/1k-M7YrSOcPEZZ8IFsORFLUt9Y7tV9xUBNOw58pByGe4/edit?usp=sharing",
-         
-         },{
-         name: "Its Too Late",
-         link: "https://drive.google.com/open?id=0B6cRrh-wbj88VEtDb1Jxc3hiY28",
-         music: "https://drive.google.com/file/d/0B6cRrh-wbj88bmlYcEI5a3N5Wm8/view?usp=sharing",
-        }
-         ];
-         
+//     });
+
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/songs", function(req, res){
-    res.render("songs",{songs, songs});
-
-});
-
+    
+    Song.find({}, function (err, allSongs) {
+        
+        if(err){
+                 console.log("Failed to retrieve Songs");
+                 
+             } else {
+                console.log("Success, created:");
+                res.render("index",{songs:allSongs});
+             }
+        });
+    });
+    
+//CREATE  /songs    POST  Add new song
 app.post("/songs", function(req, res) {
     
     var name = req.body.name;
     var link = req.body.link;
     var music = req.body.music;
-    var newSong = {name: name, link: link, music: music};
-    console.log("inPost");
-    songs.push(newSong);
+    var index = req.body.index;
+    var newSong = {name: name, link: link, music: music, index: index};
+      console.log("inPost");
+    Song.create(newSong, function (err, newlyCreated) {
+        if(err){
+                 console.log(err);
+                 
+             } else {
+                console.log(newSong);
+                res.redirect("/songs");
+             }
+        });
+    });
+  
+    // songs.push(newSong);
     //redirect back to campgrounds page. 
-    res.redirect("/songs");
-} );
+    
+// New    /songs/new GET    Display form to add dog
 
 app.get("/songs/new", function(req, res) {
     res.render("new.ejs");
 });
 
+//  SHOW    /songs/:id GET    shows info about one song
 
+app.get("/songs/:id", function(req, res) {
+    Song.findById(req.params.id, function (err, foundSong) {
+        if(err){
+            console.log(err);
+            
+        } else {
+            res.render("show", {song: foundSong});
+        
+         
+        }
+    });
+});    
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("In the Song List App");
 });
+
--- readMe.md
+++ readMe.md
@@ -11,31 +11,32 @@ Each Song has:
            { name:"name",
             link:"http://mylink.com",
             music: "http://linkToMusic.com"
-               
+            index: 0   
            },
             
             { name:"name",
             link:"http://mylink.com",
             music: "http://linkToMusic.com"
-               
+            index: 0    
            },
             { name:"name",
             link:"http://mylink.com",
             music: "http://linkToMusic.com"
-               
+            index: 0    
            },
             { name:"name",
             link:"http://mylink.com",
             music: "http://linkToMusic.com"
-               
+            index: 0    
            }
          ]
          
          * Example 
          songs[{
          name: "Girls Just want to have fun",
-         link; "https://www.youtube.com/watch?v=PIb6AZdTr-A",
-         music; "https://drive.google.com/file/d/1FrluKZhsiIQbWQILkLZUERdXlGEANP_4/view?usp=sharing",
+         link: "https://www.youtube.com/watch?v=PIb6AZdTr-A",
+         music: "https://drive.google.com/file/d/1FrluKZhsiIQbWQILkLZUERdXlGEANP_4/view?usp=sharing",
+         index: 120 
          
          },
          ]
@@ -51,10 +52,21 @@ Each Song has:
               
          #The most popular noSql db.
          #CRUD
-          Create Rea Update Destry
+          Create Reatrieeve Update Destry
           
           
           #Add Mongoose
           * Install and configure mongoose
           * Setup song model
           * Use Songs model in routes
+          
+          # Restful Routes
+          
+          name      url     verb    desc.
+         =====================================================
+          Index   /songs     GET    Display a list of all songs
+          New     /songs/new GET    Display form to add dog
+          CREATE  /songs     POST   Add new song
+          SHOW    /songs/:id GET    shows info about one song
+          
+          
